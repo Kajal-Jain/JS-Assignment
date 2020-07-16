12503f84f3b94137a06b7cc21e72a9b043cdb7bf
Kajal-Jain <68272674+Kajal-Jain@users.noreply.github.com>
2020-07-16 14:31:31 +0530

    Add files via upload

---
+++ Day2/methods.js
@@ -0,0 +1,11 @@
+//Methods of string
+
+let text="Hey there i am writing javascript.";
+console.log(text.substring(3,9)); 
+//Returns the substring at the specified location within a String object
+
+console.log(text.replace('t','1'));//Replaces text in a string, using a regular expression or search string.
+
+console.log(text.slice(3,9));//Returns a section of a string.
+
+console.log(text.search("java",3));//Finds the first substring match in a regular expression search
